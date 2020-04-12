68d9c1b815efdfc690df2bcb4c74157babc35195
Md Maruf Prodhan <mdmarufprodhan@Mds-Mac.local>
2020-04-12 13:21:00 -0700

    Add Function for home and Favourite

---
+++ MyQuotes/FavoriteViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FavoriteViewController.swift
+//  MyQuotes
+//
+//  Created by Md Maruf Prodhan on 12/4/2563 BE.
+//  Copyright © 2563 Md Maruf Prodhan. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+    
+    @IBOutlet weak var favoriteImgView: UIImageView!
+   
+    override func viewWillAppear(_ animated: Bool) {
+        let savingDefaults = UserDefaults.standard
+        let indexsaved = savingDefaults.integer(forKey: "Favorite")
+        let imageName = "quote\(indexsaved)"
+        let image = UIImage(named: imageName)
+
+        favoriteImgView.image = image
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
+
+
---
+++ MyQuotes/HomeViewController.swift
@@ -0,0 +1,65 @@
+//
+//  HomeViewController.swift
+//  MyQuotes
+//
+//  Created by Md Maruf Prodhan on 9/4/2563 BE.
+//  Copyright © 2563 Md Maruf Prodhan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController
+{
+
+    @IBOutlet weak var quoteImageView: UIImageView!
+    var quoteArry = ["quote0","quote1","quote2","quote3","quote4","quote5","quote6","quote7","quote8","quote9"]
+    var arryIndex  = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 0..<10 {
+          quoteArry.append(String(i))
+            
+        }
+
+  
+    }
+    
+    @IBAction func backClicked(_ sender: Any) {
+      
+        if arryIndex>0 {
+            arryIndex -= 1
+            
+            let imageName = quoteArry[arryIndex]
+            let quoteImage = UIImage(named: imageName)
+            quoteImageView.image = quoteImage
+            
+            
+        }
+        
+    }
+    
+ 
+    @IBAction func nextClicked(_ sender: Any) {
+        
+        if arryIndex<quoteArry.count-1{
+            arryIndex += 1
+            
+            let  imageName = quoteArry[arryIndex]
+            let quoteImage = UIImage(named: imageName)
+            quoteImageView.image = quoteImage
+            
+            
+        }
+        
+    }
+    
+    
+    @IBAction func favoriteClicked(_ sender: Any) {
+        
+        let savingDefault = UserDefaults.standard
+        savingDefault.set(arryIndex, forKey: "Favorite")
+    }
+    
+}
